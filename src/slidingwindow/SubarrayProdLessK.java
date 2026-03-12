83a8d26bc13524ba2b62367c6c67151baa83c3b1
Brijesh patra <brijeshpatra25@gmail.com>
2026-03-12 19:18:04 +0530

    Sliding window problem

--- src/slidingwindow/SubarrayProdLessK.java
+++ src/slidingwindow/SubarrayProdLessK.java
@@ -4,8 +4,7 @@ public class SubarrayProdLessK {
     public int numSubArrayProductLessThanK(int[]nums,int k){
         if (k<=1)return 0;
 
-        int left=0;
-        int product=1;
+        int left=0;int product=1;
         int count=0;
 
         int n=nums.length;
